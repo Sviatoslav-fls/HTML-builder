1d09156a80fd6667f5fc7597e384da064a2eea9a
Sviatoslav-fls <msv.fls@gmail.com>
2021-11-03 17:24:03 +0200

    feat: add solution 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, 'info.txt');
+const writeStream = fs.createWriteStream(filePath);
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Hi! What\'s your name?\n', (answer) => {
+  console.log(`Nice to meet you, ${answer}!\nEnter your favorite artists or enter 'exit' for quit.`);
+  rl.prompt();
+});
+
+rl.on('line', (answer) => {
+  if (answer == 'exit') completionProcess();
+
+  writeStream.write(answer + '\n');
+  console.log('Your answer was written.');
+  console.log('Enter one more artists or enter \'exit\' for quit.');
+  rl.prompt();
+});
+
+rl.on('SIGINT', completionProcess);
+
+function completionProcess() {
+  console.log('GoodBye!\nProcess exited.');
+  process.exit(0);
+}
