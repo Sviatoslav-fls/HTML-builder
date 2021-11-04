708f8254a3e834a8de77cbe214735f9440d23065
Sviatoslav-fls <msv.fls@gmail.com>
2021-11-04 02:02:54 +0200

    feat: add solution 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, 'secret-folder');
+
+function getFilesFromDirectory(dir) {
+  fs.readdir(dir, (err, data) => {
+    err ? console.log(err) : null;
+
+    data.forEach(file => {
+      const route = path.join(dir, file);
+
+      fs.stat(route, (err, stats) => {
+        err ? console.log(err) : null;
+
+        if (stats.isFile()) {
+          const name = path.parse(file).name;
+          const extname = path.extname(file).slice(1);
+          const size = (stats.size / 1024).toFixed(3) + 'kb';
+
+          console.log(`${name} - ${extname} - ${size}`);
+        } else {
+          getFilesFromDirectory(route);
+        }
+      });
+    });
+  });
+}
+getFilesFromDirectory(filePath);
